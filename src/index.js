0089d370ede7190a0f357ccf67585c6c496b9aa0
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2022-08-14 19:33:34 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,99 +1,126 @@
-/** @format */
-"use strict"
-
 class kNow {
-	constructor(defaultNextTimeout) {
-		this.persistentHandlers = {};
-		this.dispatchWatchers = {};
-		this.defaultNextTimeout = defaultNextTimeout ?? 2147483647;
-		this.allowPromises = "Promise" in window;
-	}
-	when(signalIdentifier, callback) {
-		if (!(signalIdentifier in this.persistentHandlers)) {
-			this.persistentHandlers[signalIdentifier] = [];
-		}
-		var instanceID = math.random().toString().slice(2);
-		this.persistentHandlers[signalIdentifier].push({
-			method : callback,
-			instanceID
-		});
-		return instanceID;
-	}
-	clearWhen(type, ID) {
-		if (type) {
-			if (!this.persistentHandlers[type]) return;
-			if (type && ID) {
-				var implicatedIndex = this.persistentHandlers[type].reduce((last, entry, index) => return entry.instanceID === id ? index : last, null);
-				if (implicatedIndex) {
-					this.persistentHandlers[type].splice(index, 1);
-				}
-				return;
-			}
-			this.persistentHandlers[type] = [];
-			return;
-		}
-		this.persistentHandlers = {};
-	}
-	dispatch(signalIdentifier, externalDetail) {
-		if (this.persistentHandlers[signalIdentifier]) {
-			this.persistentHandlers[signalIdentifier].forEach((member) => method(externalDetail));
-		}
-		if (this.dispatchWatchers[signalIdentifier]) {
-			this.dispatchWatchers[signalIdentifier].forEach((watcher) =>
-				watcher.resolve({ externalDetail: externalDetail, signalIdentifier })
-			);
-			delete this.dispatchWatchers[signalIdentifier];
-		}
-	}
-	forceReject(signalIdentifier, reason) {
-		if (!this.dispatchWatchers[signalIdentifier]) {
-			this.dispatchWatchers[signalIdentifier].forEach((watcher) => watcher.reject(reason));
-			delete this.dispatchWatchers[signalIdentifier];
-		}
-	}
-	async next(dispatchIdentifier, timeout) {
-		if (!this.allowPromises)
-			throw new Error(
-				"The current environment doesn't support promises; cannot attatch 'next'-style listener."
-			);
-		if (!this.dispatchWatchers[dispatchIdentifier]) this.dispatchWatchers[dispatchIdentifier] = [];
-		var rejectGeneratedPromise;
-		var resolveGeneratedPromise;
-		var index =
-			this.dispatchWatchers[dispatchIdentifier].push({
-				promise: new Promise((resolve, reject) => {
-					var spent = false;
-					resolveGeneratedPromise = async (resolution) => {
-						if (spent) return;
-						resolve(resolution);
-						spent = true;
-					};
-					rejectGeneratedPromise = async (rejection) => {
-						if (spent) return;
-						reject(rejection);
-						spent = true;
-					};
-					setTimeout(() => {
-						if (spent) return;
-						reject(`Listener timed out after ${timeout ?? this.defaultNextTimeout}ms`);
-					}, timeout ?? this.defaultNextTimeout);
-				}),
-				reject: rejectGeneratedPromise,
-				resolve: resolveGeneratedPromise,
-			}) - 1;
-		return await this.dispatchWatchers[dispatchIdentifier][index].promise;
-	}
-	clearNext(type, method) {
-		var flushFunction =
-			method === "resolve" || method === "reject"
-				? method === "resolve"
-					? this.dispatch
-					: this.forceReject
-				: this.dispatch;
-		for (i in this.dispatchWatchers) {
-			flushFunction(i, "flushed");
-		}
-	}
+  constructor(defaultNextTimeout) {
+    this.persistentHandlers = {};
+    this.dispatchWatchers = {};
+    this.defaultNextTimeout =
+      defaultNextTimeout !== undefined && defaultNextTimeout !== null
+        ? defaultNextTimeout
+        : 2147483647;
+    this.allowPromises = "Promise" in window;
+  }
+  when(signalIdentifier, callback) {
+    if (!(signalIdentifier in this.persistentHandlers)) {
+      this.persistentHandlers[signalIdentifier] = [];
+    }
+    var instanceID = Math.random().toString().slice(2);
+    this.persistentHandlers[signalIdentifier].push({
+      method: callback,
+      instanceID
+    });
+    return instanceID;
+  }
+  clearWhen(type, ID) {
+    if (type) {
+      if (!this.persistentHandlers[type]) return;
+      if (type && ID) {
+        var implicatedIndex = this.persistentHandlers[type].reduce(
+          (last, entry, index) => (entry.instanceID === ID ? index : last),
+          null
+        );
+        if (implicatedIndex) {
+          this.persistentHandlers[type].splice(implicatedIndex, 1);
+        }
+        return;
+      }
+      this.persistentHandlers[type] = [];
+      return;
+    }
+    this.persistentHandlers = {};
+  }
+  dispatch(signalIdentifier, externalDetail) {
+    if (this.persistentHandlers[signalIdentifier]) {
+      this.persistentHandlers[signalIdentifier].forEach((member) =>
+        member.method(externalDetail)
+      );
+    }
+    if (this.dispatchWatchers[signalIdentifier]) {
+      this.dispatchWatchers[signalIdentifier].forEach((watcher) =>
+        watcher.resolve({ externalDetail: externalDetail, signalIdentifier })
+      );
+      delete this.dispatchWatchers[signalIdentifier];
+    }
+  }
+  forceReject(signalIdentifier, reason) {
+    if (!this.dispatchWatchers[signalIdentifier]) {
+      this.dispatchWatchers[signalIdentifier].forEach((watcher) =>
+        watcher.reject(reason)
+      );
+      delete this.dispatchWatchers[signalIdentifier];
+    }
+  }
+  async next(dispatchIdentifier, timeout) {
+    if (!this.allowPromises)
+      throw new Error(
+        "The current environment doesn't support promises; cannot attatch 'next'-style listener."
+      );
+    if (!this.dispatchWatchers[dispatchIdentifier])
+      this.dispatchWatchers[dispatchIdentifier] = [];
+    var rejectGeneratedPromise;
+    var resolveGeneratedPromise;
+    var index =
+      this.dispatchWatchers[dispatchIdentifier].push({
+        promise: new Promise((resolve, reject) => {
+          var spent = false;
+          resolveGeneratedPromise = async (resolution) => {
+            if (spent) return;
+            resolve(resolution);
+            spent = true;
+          };
+          rejectGeneratedPromise = async (rejection) => {
+            if (spent) return;
+            
+            //
+            //
+            
+            
+            reject(rejection);
+            
+            ///
+            
+            spent = true;
+          };
+          setTimeout(
+            () => {
+              if (spent) return;
+              reject(
+                `Listener timed out after ${
+                  timeout !== undefined && timeout !== null
+                    ? timeout
+                    : this.defaultNextTimeout
+                }ms`
+              );
+            },
+            timeout !== undefined && timeout !== null
+              ? timeout
+              : this.defaultNextTimeout
+          );
+        }),
+        reject: rejectGeneratedPromise,
+        resolve: resolveGeneratedPromise
+      }) - 1;
+    return await this.dispatchWatchers[dispatchIdentifier][index].promise;
+  }
+  clearNext(type, method) {
+    var flushFunction =
+      method === "resolve" || method === "reject"
+        ? method === "resolve"
+          ? this.dispatch
+          : this.forceReject
+        : this.dispatch;
+    for (var i in this.dispatchWatchers) {
+      flushFunction(i, "flushed");
+    }
+  }
 }
 
-export default kNow;
+window.kNow = kNow;
